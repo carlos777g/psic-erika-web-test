38722bc8ab8ea382387c5b3246a150032789c8b3
Carlos Guillén <gcarlosjael@gmail.com>
2025-02-24 22:33:14 -0600

    Cambio en la barra de navegación (ahora se esconde al scrollear), formato de documentos y corrección de rutas para manejo general desde cualquier otra ruta

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,27 +1,27 @@
 export const renderHome = (req, res) => {
     res.render('index', { 
       title: 'Inicio - PsicErika', 
-      pageCSS: 'css/index.css' 
+      pageCSS: '/css/index.css' 
     });
   };
   
   export const renderAbout = (req, res) => {
     res.render('about', { 
       title: 'Sobre mí - PsicErika', 
-      pageCSS: 'css/about.css' 
+      pageCSS: '/css/about.css' 
     });
   };
   
   export const renderPsiconfort = (req, res) => {
     res.render('psiconfort', { 
       title: 'Psiconfort - PsicErika', 
-      pageCSS: 'css/psiconfort.css' 
+      pageCSS: '/css/psiconfort.css' 
     });
   };
   
   export const renderTerapiaOnline = (req, res) => {
     res.render('terapia_online', { 
       title: 'Terapia Online - PsicErika', 
-      pageCSS: 'css/contact.css' 
+      pageCSS: '/css/contact.css' 
     });
   };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,16 @@
-import { Router } from 'express';
-import { 
-  renderHome, 
-  renderAbout, 
-  renderPsiconfort, 
-  renderTerapiaOnline 
-} from '../controllers/indexController.js';
+import { Router } from "express";
+import {
+  renderHome,
+  renderAbout,
+  renderPsiconfort,
+  renderTerapiaOnline,
+} from "../controllers/indexController.js";
 
 const router = Router();
 
-router.get('/', renderHome);
-router.get('/about', renderAbout);
-router.get('/psiconfort', renderPsiconfort);
-router.get('/terapia_online', renderTerapiaOnline);
+router.get("/", renderHome);
+router.get("/about", renderAbout);
+router.get("/psiconfort", renderPsiconfort);
+router.get("/terapia_online", renderTerapiaOnline);
 
 export default router;
-
-
-
